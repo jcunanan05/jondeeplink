2e298429d99f22eb906c0d71d86345b442bb85ee
jcunanan05 <cunanan.jonathan1995@gmail.com>
2021-06-02 17:59:37 -0400

    Open hello screen when you visit deeplink/hello

--- AppContainer.js
+++ AppContainer.js
@@ -1,15 +1,32 @@
 import * as React from 'react';
+import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import App from './App';
+import { name as appName } from './app.json';
+import HelloScreen from './HelloScreen';
 
 const Stack = createStackNavigator();
 
+const linking = {
+  prefixes: [`${appName}://`],
+  config: {
+    screens: {
+      Hello: 'hello/:id',
+    },
+  },
+};
+
 export default function AppContainer() {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking} fallback={<Text>oh no</Text>}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={App} options={{ title: 'Home' }} />
+        <Stack.Screen
+          name="Hello"
+          component={HelloScreen}
+          options={{ title: 'Hello' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ HelloScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export default function HelloScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Hello screen!</Text>
+    </SafeAreaView>
+  );
+}
